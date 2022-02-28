0373c21be15b78734ee96c83c627fdfbadf1fea0
HP-03 <HP-03@HP-3>
2022-02-28 19:44:30 +0100

    Inicio manual GitHub

---
+++ Manual/src/manual/Manual.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package manual;
+
+/**
+ *
+ * @author HP-03
+ */
+public class Manual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Inicio de manual Github");
+        boolean manual = true;
+        if (manual==true){
+            System.out.println("Vamos con todo");
+        }   else {
+            System.out.println("Se acabó");
+            }
+    }
+    
+}
